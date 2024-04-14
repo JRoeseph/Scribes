a54819d427379f20018108422d5a58c3ef72b281
JRoeseph <48298846+JRoeseph@users.noreply.github.com>
2024-04-14 16:33:21 -0700

    Fixed more styling, added TODOs on temporary code blocks

--- scripts/base_tile.gd
+++ scripts/base_tile.gd
@@ -4,12 +4,12 @@ extends Button
 ##
 ## This class handles the rendering and storage of all the information a tile has
 
-
 ## Called when the class is initialized
 func init_class(char, val) -> BaseTile:
 	$Character.text = char
 	$Value.text = val
 	return self
+
 
 ## Initializes the class with default values according to Scrababa
 func default_init(char) -> BaseTile:
@@ -17,12 +17,15 @@ func default_init(char) -> BaseTile:
 	$Value.text = str(get_default_value(char))
 	return self
 
+
+# TODO: Remove once system is more robust
 var default_vals = {"A":1,"B":3,"C":3,"D":2,"E":1,"F":4,
 					"G":2,"H":4,"I":1,"J":8,"K":5,"L":1,
 					"M":3,"N":1,"O":1,"P":3,"Q":10,"R":1,
 					"S":1,"T":1,"U":1,"V":4,"W":4,"X":8,"Y":4,"Z":10}
 func get_default_value(char):
 	return default_vals[char]
+
 
 ## Called when a tile is clicked
 func _on_button_down() -> void:
--- scripts/board_space.gd
+++ scripts/board_space.gd
@@ -11,12 +11,14 @@ var placed_tile: BaseTile = null :
 		return placed_tile
 	set(value):
 		placed_tile = value
+		
 ## Whether or not the space is locked for future grabbing
 var is_locked: bool = false :
 	get:
 		return is_locked
 	set(value):
 		is_locked = value
+
 
 ## Called when the tile on the BoardSpace is pressed
 func tile_pressed(tile: BaseTile) -> void:
@@ -25,15 +27,20 @@ func tile_pressed(tile: BaseTile) -> void:
 		get_parent().get_parent().grabbed_tile = tile
 		placed_tile = null
 
+
 ## Places a tile into this space
 func place_tile(tile: BaseTile) -> void:
 	placed_tile = tile
 	add_child(placed_tile)
 	get_parent().get_parent().grab_tile_hover_tween.kill()
 	var tween: Tween = get_tree().create_tween().bind_node(self).set_trans(Tween.TRANS_SINE)
-	#tween.parallel().tween_property(placed_tile, "position", Vector2(15, 15), 0)
-	#tween.parallel().tween_property(placed_tile, "scale", Vector2(1, 1), 0)
-	#tween.parallel().tween_property(placed_tile, "rotation", 0, 0)
-	placed_tile.position = Vector2(15,15)
-	placed_tile.scale = Vector2(1,1)
+	# TODO: These can help with the snapping bug, but the issue is the default position of the tile
+	# when it becomes a child of the space makes it have strange behavior. If you can set the tile
+	# position, rotation, and scale to what it is on the main environment, but as a child of the
+	# space, then execute these, it would fix the problem 
+	#tween.parallel().tween_property(placed_tile, "position", Vector2(15, 15), 0.2)
+	#tween.parallel().tween_property(placed_tile, "scale", Vector2(1, 1), 0.2)
+	#tween.parallel().tween_property(placed_tile, "rotation", 0, 0.2)
+	placed_tile.position = Vector2(15, 15)
+	placed_tile.scale = Vector2(1, 1)
 	placed_tile.rotation = 0
